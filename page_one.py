6131fe378ad04d183a41d16ea76b11d3fc500814
GiulioCampina <93941029+GiulioCampina@users.noreply.github.com>
2026-02-14 14:23:37 +0100

    Update page_one.py

--- page_one.py
+++ page_one.py
@@ -6,10 +6,11 @@ st.set_page_config(initial_sidebar_state="collapsed")
 adrs_file = os.path.abspath(__file__)
 
 st.title("Bienvenue sur :red[reko] ")
-st.markdown(":orange[connecte] toi a ton profil ou :rainbow[inscrit] toi!", )
+st.markdown(":orange[connecte] toi a ton profil ou :rainbow[inscris] toi!", )
 
 if st.button(":rainbow[inscris toi]", icon="📝"):
     st.switch_page("pages/insc_toi.py")
 
 if st.button(":orange[connecte toi]", icon="🔥"):
+
     st.switch_page("pages/cnx_pge.py")
